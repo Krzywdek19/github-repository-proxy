ae3e78cacd635927fc4bca46fd9eca468dfe9735
Kuba <krzywdun@wp.pl>
2026-05-27 10:46:52 +0200

    Initialize Spring Bood project

---
+++ src/main/java/pl/exceptionhandled/github_repository_proxy/GithubRepositoryProxyApplication.java
@@ -0,0 +1,13 @@
+package pl.exceptionhandled.github_repository_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoryProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/exceptionhandled/github_repository_proxy/GithubRepositoryProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.exceptionhandled.github_repository_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
